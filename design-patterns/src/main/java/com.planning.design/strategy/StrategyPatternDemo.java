cb6165fdc8ca169a5229c32dadfa778824a67873
planning018 <13057588636@163.com>
2019-10-29 08:54:24 +0800

    Strategy Pattern Demo

---
+++ design-patterns/src/main/java/com.planning.design/strategy/StrategyPatternDemo.java
@@ -0,0 +1,19 @@
+package com.planning.design.strategy;
+
+/**
+ * @author planning
+ * @create 2019-10-28 21:14
+ **/
+public class StrategyPatternDemo {
+
+    public static void main(String[] args) {
+        Context context = new Context(new OperationAdd());
+        System.out.println("10 + 5 = " + context.executeStrategy(10, 5));
+
+        context = new Context(new OperatorSubtraction());
+        System.out.println("10 - 5 = " + context.executeStrategy(10, 5));
+
+        context = new Context(new OperationMultiply());
+        System.out.println("10 * 5 = " + context.executeStrategy(10, 5));
+    }
+}
