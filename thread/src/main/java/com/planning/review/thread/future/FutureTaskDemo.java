8f48e14255303f6a005cef9706c3107ba2338a04
planning018 <13057588636@163.com>
2020-04-23 07:34:48 +0800

    FutureTask demo 优化

--- thread/src/main/java/com/planning/review/thread/future/FutureTaskDemo.java
+++ thread/src/main/java/com/planning/review/thread/future/FutureTaskDemo.java
@@ -1,33 +1,39 @@
 package com.planning.review.thread.future;
 
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.FutureTask;
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+
+import java.util.concurrent.*;
 
 /**
  * 学习一下 FutureTask 的用法
+ *
+ * @author planning 2019.09.23
  */
 public class FutureTaskDemo {
 
+    private static ThreadPoolExecutor executor = new ThreadPoolExecutor(4, 4, 10L, TimeUnit.SECONDS, new LinkedBlockingDeque<>(),
+            new ThreadFactoryBuilder().setNameFormat("planning-thread-%d").build());
+
     public static void main(String[] args) {
-        Callable callable = () -> {
+
+        // 在主线程中，启动 FutureTask 任务
+        FutureTask<String> futureTask = new FutureTask<String>(() -> {
             System.out.println("begin to cook food");
             Thread.sleep(5000);
             return "food cook is ok";
-        };
-
-        FutureTask<String> futureTask = new FutureTask<String>(callable);
-        new Thread(futureTask).start();
+        });
+        // 提交到 线程池 中执行
+        executor.submit(futureTask);
         System.out.println("begin to make coke");
         try {
-            // assume make cook cost 3000ms
+            // 主线程等待： assume make cook cost 3000ms
             Thread.sleep(3000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         System.out.println("coke is ok");
 
-        if(!futureTask.isDone()){
+        if (!futureTask.isDone()) {
             System.out.println("food is not ok, please wait 3000 ms");
             try {
                 Thread.sleep(3000);
