9f3cc04f4467c8cc63fae713f87b77f64bd51d7c
planning018 <13057588636@163.com>
2020-05-29 07:35:52 +0800

    FutureTask demo 优化

--- throwable/src/test/java/com/yeyangmei/test/Test01.java
+++ throwable/src/test/java/com/yeyangmei/test/Test01.java
@@ -19,35 +19,35 @@ public class Test01 {
     private static Logger logger = LoggerFactory.getLogger(Test01.class);
 
     // character replace
-    private String replace(String truckLength, String occupyTruckLength){
+    private String replace(String truckLength, String occupyTruckLength) {
         // 场景1： 3.4,4.7,8 ---> 3.4/4.7/8米
         //String str1 = "3.4,4.7,8";
 
         String truckLengthResult = dealTruckLength(truckLength);
 
         String occupyTruckLengthResult = "";
-        if(!StringUtils.isBlank(occupyTruckLength)){
+        if (!StringUtils.isBlank(occupyTruckLength)) {
             occupyTruckLengthResult = "/占_米".replace("_", occupyTruckLength);
         }
 
-        if(StringUtils.isBlank(truckLengthResult)){
-            occupyTruckLengthResult = occupyTruckLengthResult.replace("/","");
+        if (StringUtils.isBlank(truckLengthResult)) {
+            occupyTruckLengthResult = occupyTruckLengthResult.replace("/", "");
         }
 
         return truckLengthResult + occupyTruckLengthResult;
     }
 
-    private String dealTruckLength(String truckLength){
-        if(StringUtils.isBlank(truckLength)){
+    private String dealTruckLength(String truckLength) {
+        if (StringUtils.isBlank(truckLength)) {
             logger.info("字符串不合法");
             return "";
         }
 
-        if("-1".equals(truckLength)){
+        if ("-1".equals(truckLength)) {
             return "不限车长";
         }
 
-        return truckLength.replace(",","/") + "米";
+        return truckLength.replace(",", "/") + "米";
 
 /*        List<String> str1List = Arrays.asList(truckLength.split(","));
         if(str1List.contains("-1")){
@@ -78,16 +78,16 @@ public class Test01 {
         }
 
         if (StringUtils.isNotBlank(occupyTruckLength)) {
-            lengthList.add("占_米".replace("_",occupyTruckLength));
+            lengthList.add("占_米".replace("_", occupyTruckLength));
         }
 
-        return StringUtils.join(lengthList.stream().filter(StringUtils :: isNotBlank).collect(Collectors.toList()), "/");
+        return StringUtils.join(lengthList.stream().filter(StringUtils::isNotBlank).collect(Collectors.toList()), "/");
     }
 
     @Test
-    public void testSubstring(){
+    public void testSubstring() {
         String str = "hello";
-        System.out.println(str.substring(0,3));
+        System.out.println(str.substring(0, 3));
 
         System.out.println("result is :" + 12432311 % 100);
         System.out.println("result is :" + 12432312 / 100);
@@ -95,15 +95,15 @@ public class Test01 {
     }
 
     @Test
-    public void testListCompare(){
-        List<Integer> list1 = Arrays.asList(1,2,3);
-        List<Integer> list2 = Arrays.asList(2,3);
+    public void testListCompare() {
+        List<Integer> list1 = Arrays.asList(1, 2, 3);
+        List<Integer> list2 = Arrays.asList(2, 3);
 
         System.out.println(list1.containsAll(list2));
     }
 
     @Test
-    public void test(){
+    public void test() {
         String truckLength = "1.2,2.3,4";
         String truckLength1 = "-1";
         String truckLength2 = "";
@@ -120,24 +120,37 @@ public class Test01 {
     }
 
     @Test
-    public void testObject(){
-        Person p1 = new Person("zhangsan","123");
+    public void testObject() {
+        Person p1 = new Person("zhangsan", "123");
         p1 = changePerson(p1, true);
         System.out.println(p1.toString());
     }
 
-    private Person changePerson(Person p1, boolean judge){
-        if(judge){
-            Person p2 = new Person("lisi","234");
+    private Person changePerson(Person p1, boolean judge) {
+        if (judge) {
+            Person p2 = new Person("lisi", "234");
             return p2;
         }
         return p1;
     }
 
     @Test
-    public void test11(){
-        Map map = new HashMap();
-        System.out.println(map.values());
+    public void test11() {
+//        Map map = new HashMap();
+//        System.out.println(map.values());
+
+        System.out.println(hailstone(27));
+        System.out.println(hailstone(42));
+    }
+
+    private int hailstone(int n) {
+        int length = 1;
+        while (n > 1) {
+            n = n % 2 == 0 ? n / 2 : 3 * n + 1;
+            System.out.print(n+ " ");
+            length++;
+        }
+        return length;
     }
 
 }
