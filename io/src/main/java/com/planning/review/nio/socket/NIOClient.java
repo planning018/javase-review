b1c538e8654688b18311c4482e041410ea3abcd5
xingcheng.yin <13057588636@163.com>
2020-08-05 21:00:05 +0800

    nio socket demo

---
+++ io/src/main/java/com/planning/review/nio/socket/NIOClient.java
@@ -0,0 +1,44 @@
+package com.planning.review.nio.socket;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.Buffer;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+import java.util.Scanner;
+
+/**
+ * @author yxc
+ * @since 2020-08-05 11:16
+ **/
+public class NIOClient {
+
+    public static void main(String[] args) throws Exception {
+        // 1. 得到一个网络通道
+        SocketChannel channel = SocketChannel.open();
+        // 2. 设置非阻塞方式
+        channel.configureBlocking(false);
+        // 3. 提供服务器端的 IP 地址和端口
+        InetSocketAddress address = new InetSocketAddress("127.0.0.1", 9999);
+        // 4. 连接服务器
+        if(!channel.connect(address)){
+            // nio 非阻塞的优势
+            while(!channel.finishConnect()){
+                System.out.println("Client: 连接服务器的同时，还可以做一些额外的事情");
+            }
+        }
+        // 5. 得到一个缓冲区并存入数据
+/*        String msg = "hello, nio server";
+        ByteBuffer buffer = ByteBuffer.wrap(msg.getBytes());
+        // 6. 发送数据
+        channel.write(buffer);*/
+
+        Scanner scanner = new Scanner(System.in);
+        String msg = scanner.nextLine();
+        while(scanner.hasNextLine()){
+            ByteBuffer buffer = ByteBuffer.wrap(msg.getBytes());
+            // 6. 发送数据
+            channel.write(buffer);
+        }
+    }
+}
