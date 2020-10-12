3217aa13c750ed28e30d0bb737ab341aba215c6d
xingcheng.yin <13057588636@163.com>
2020-10-12 16:11:43 +0800

    stream file demo

---
+++ container/src/main/java/com/planning/stream/Cake.java
@@ -0,0 +1,17 @@
+package com.planning.stream;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * @author yxc
+ * @since 2020-10-12 15:00
+ **/
+@Data
+@AllArgsConstructor
+public class Cake {
+
+    private int id;
+    private String name;
+    private int price;
+}
