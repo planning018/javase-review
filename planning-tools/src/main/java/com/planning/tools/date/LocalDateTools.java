6d2c1c512760b10f1f888937af8f09fbb601f6ce
planning018 <13057588636@163.com>
2021-01-19 15:02:38 +0800

    LocalDateTime tool

--- planning-tools/src/main/java/com/planning/tools/date/LocalDateTools.java
+++ planning-tools/src/main/java/com/planning/tools/date/LocalDateTools.java
@@ -13,28 +13,23 @@ import java.util.Date;
 public class LocalDateTools {
 
     public static void main(String[] args) {
+        // 获取当前时间（毫秒）
         System.out.println(LocalDateTime.now().toInstant(ZoneOffset.of("+8")).toEpochMilli());
-
-        System.out.println(System.currentTimeMillis());
-
         System.out.println(LocalDateTime.of(LocalDate.now(), LocalTime.MAX).atZone(ZoneId.systemDefault()).toInstant().toEpochMilli());
 
-        // 当天的 0点 - 24点
+        // 当天的 0点 - 24点（秒）
         System.out.println(LocalDateTime.of(LocalDate.now(), LocalTime.MIN).toEpochSecond(ZoneOffset.of("+8")));
         System.out.println(LocalDateTime.of(LocalDate.now(), LocalTime.MAX).toEpochSecond(ZoneOffset.of("+8")));
 
+        // 当天的 0点 - 24点（毫秒）
         System.out.println(new Date(LocalDateTime.of(LocalDate.now(), LocalTime.MAX).toInstant(ZoneOffset.of("+8")).toEpochMilli()));
         System.out.println(new Date(LocalDateTime.of(LocalDate.now(), LocalTime.MAX).toEpochSecond(ZoneOffset.of("+8")) * 1000));
 
-
+        // 格式化时间
         System.out.println(LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
 
+        // 当前时间往前推 5 分钟
+        System.out.println(LocalDateTime.now().minusMinutes(5).toInstant(ZoneOffset.of("+8")).toEpochMilli());
     }
 
-
-    public void todayMinMaxTime(){
-        // 当天的 0点 - 24点()
-        System.out.println(LocalDateTime.of(LocalDate.now(), LocalTime.MIN).toEpochSecond(ZoneOffset.of("+8")));
-        System.out.println(LocalDateTime.of(LocalDate.now(), LocalTime.MAX).toEpochSecond(ZoneOffset.of("+8")));
-    }
 }
