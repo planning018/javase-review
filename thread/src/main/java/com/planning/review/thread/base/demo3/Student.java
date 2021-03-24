265117b3d59480423c5c20e993435ad8ca9fc276
planning018 <13057588636@163.com>
2021-03-24 14:34:36 +0800

    代码重构

--- thread/src/main/java/com/planning/review/thread/base/demo3/Student.java
+++
@@ -1,8 +0,0 @@
-package com.planning.review.thread.base.demo3;
-
-public class Student {
-
-	 String name;
-	 int age;
-	 boolean flag = false;
-}
---
+++ thread/src/main/java/com/planning/thread/basic/notify/Student.java
@@ -0,0 +1,8 @@
+package com.planning.thread.basic.notify;
+
+public class Student {
+
+	 String name;
+	 int age;
+	 boolean flag = false;
+}
