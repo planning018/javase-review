b11b5cea1c3231975f972dc410f1bfe183c35ff0
planning018 <13057588636@163.com>
2021-05-11 14:55:54 +0800

    list join format

--- container/src/test/java/com/planning/container/test/StreamFunctionTest.java
+++ container/src/test/java/com/planning/container/test/StreamFunctionTest.java
@@ -1,7 +1,7 @@
 package com.planning.container.test;
 
 import com.alibaba.fastjson.JSON;
-import org.assertj.core.util.Lists;
+import com.google.common.collect.Lists;
 import org.junit.Test;
 
 import java.util.List;
@@ -23,4 +23,17 @@ public class StreamFunctionTest {
         filterValue(numList);
         System.out.println(JSON.toJSONString(numList));
     }
+
+    @Test
+    public void testListsJoin(){
+        List<Integer> startCityList = Lists.newArrayList(100000,100001,100002);
+        List<Integer> endCityList = Lists.newArrayList(200000,200001,200002);
+
+        String format = "%s_%s";
+
+        // expect: 100000_200000,100000_200001,...,100002_200002
+        String result = startCityList.stream().flatMap(start -> endCityList.stream().map(end -> String.format(format, start, end))).collect(Collectors.joining(","));
+        System.out.println(result);
+    }
+
 }
